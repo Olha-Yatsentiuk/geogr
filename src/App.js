93b8acb5fe2a238be3de1228b9e636708a6a39ac
Olha Yatsentiuk <Olha_Yatsentiuk@epam.com>
2021-09-22 12:55:12 +0300

    m

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+
+export default function App() {
+  return (
+    <div class = "container">
+      <img class = "logo" src = "logo.png" alt = "logo"></img>
+      <h1>Кафедра географії </h1>
+      <img class = "face" src = "3.jpg" alt = "face"></img>
+      <div class= "info">
+        <p> Шановні випускники, якщо Ви любите подорожувати, запрошуємо Вас на 
+          навчання на кафедру географії Вінницького державного педагогічного 
+          університету. </p>      
+       <ul>  Спеціальності: 
+        <li class = "speciality">«Середня освіта. Географія» (спеціалізація «Краєзнавчо-туристична робота»); </li>
+           <ol> Сертифікати ЗНО:
+            <li> Українська мова та література </li>
+            <li> Географія </li>
+            <li> Історія України або математика, або іноземна мова, або біологія, або фізика, або хімія </li>
+          </ol>
+        <li class = "speciality">«Науки про Землю» (спеціалізація «Конструктивна географія»);</li>
+          <ol> Сертифікати ЗНО:
+            <li>Українська мова </li>
+            <li> Математика </li>
+            <li> Історія України або іноземна мова, або біологія, або географія, або фізика, або хімія </li>
+          </ol>
+        <li class = "speciality">"Туризм" (не вчитель!).</li>
+          <ol>Сертифікати ЗНО:
+            <li>Українська мова та література </li>
+            <li> Іноземна мова </li>
+            <li> Історія України або математика, або біологія, або географія, або фізика, або хімія </li>
+          </ol>
+      </ul>
+        <p> Гарантуємо Вам незабутні враження, кваліфіковану підготовку та практичні знання. </p>
+      </div>
+
+      <div class = "photos">
+        <img class = "gallery" src = "1.jpg" alt = "students"></img>
+        <img class = "gallery" src = "2.jpg" alt = "students"></img>
+        <img class = "gallery" src = "3.jpg" alt = "students"></img>
+        <img class = "gallery" src = "4.jpg" alt = "students"></img>
+        <img class = "gallery" src = "5.jpg" alt = "students"></img>
+        <img class = "gallery" src = "6.jpg" alt = "students"></img>
+        <img class = "gallery" src = "7.jpg" alt = "students"></img>
+        <img class = "gallery" src = "8.jpg" alt = "students"></img>
+        <img class = "gallery" src = "9.jpg" alt = "students"></img>
+        <img class = "gallery" src = "10.jpg" alt = "students"></img>
+        <img class = "gallery" src = "11.jpg" alt = "students"></img>
+        <img class = "gallery" src = "12.jpg" alt = "students"></img>
+
+      </div>
+      <p class = "contact">За довідками з питань вступу звертайтесь за телефонами 067-770-78-56; Viber 093-856-39-59 . </p>
+    </div>
+    
+
+  );
+}
